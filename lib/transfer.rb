8633f84cf4a89d94b256dd42f80149aa8edb9970
Diane Crame <dcrame@icloud.com>
2020-04-07 18:16:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,14 +13,14 @@ class Transfer
   end
   
    def execute_transaction
-    if sender.balance < @amount || !self.valid? 
-       self.status= "rejected"
-       "Transaction rejected. Please check your account balance."
-     else
-    sender.balance-=amount
-    receiver.deposit(amount)
-    self.status= "complete"
-     end
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
   end
 
   def reverse_transfer
